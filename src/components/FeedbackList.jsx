c5bc4ee3a1fb29e13d03d2e302b8ac7ff0425c01
harsh panchal <harshpanchal.silverwing@gmail.com>
2022-02-07 19:48:40 -0500

    Animation for lists added

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,26 +1,46 @@
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
-function FeedbackList({ feedback,handleDelete }) {
+import { motion, AnimatePresence } from "framer-motion";
+
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No data found</p>;
   } else {
     return (
       <div className="feedback-list">
+        <AnimatePresence>
         {feedback.map((item, index) => (
-          <FeedbackItem 
-          key={index} 
-          itemData={item} 
-          handleDelete={handleDelete}
-        />
+         <motion.div
+            key={item.id}
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            exit={{opacity:0}}
+         >
+            <FeedbackItem
+            key={index}
+            itemData={item}
+            handleDelete={handleDelete}
+          />
+         </motion.div>
         ))}
+        </AnimatePresence>
       </div>
+      // <div className="feedback-list">
+      //   {feedback.map((item, index) => (
+      //     <FeedbackItem
+      //       key={index}
+      //       itemData={item}
+      //       handleDelete={handleDelete}
+      //     />
+      //   ))}
+      // </div>
     );
   }
 }
-FeedbackList.propTypes = {
-  feedback: PropTypes.array.isRequired
-}
 
+FeedbackList.propTypes = {
+  feedback: PropTypes.array.isRequired,
+};
 
 export default FeedbackList;
